304f0d5a332e87c4174e8683c2c856d7e2409e19
zulma <zumendez7@gmail.com>
2021-03-31 16:21:35 +0200

    modifications

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -7,18 +7,18 @@
     <title>Document</title>
 
     <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href={{asset('vendor/bootstrap/css/bootstrap.min.css')}} rel="stylesheet">
 
   <!-- Custom fonts for this template -->
   <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
 
   <!-- Custom styles for this template -->
-  <link href="css/business-casual.min.css" rel="stylesheet">
+  <link href={{asset('css/business-casual.min.css')}} rel="stylesheet">
+  <link rel="stylesheet" href={{asset('css/app.css')}}>
 
 </head>
 <body>
-    
     <h1 class="site-heading text-center text-white d-none d-lg-block">
         <span class="site-heading-upper text-primary mb-3">A Free Bootstrap 4 Business Theme</span>
         <span class="site-heading-lower">Business Casual</span>
@@ -37,6 +37,7 @@
     <!-- Bootstrap core JavaScript -->
   <script src="vendor/jquery/jquery.min.js"></script>
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src={{asset('js/app.js')}}></script>
 
 </body>
 </html>
